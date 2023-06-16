8aa9054ee0fc6c9374f9761621160ffb2ce28b27
Morgan Mutila <mutilamorgan@gmail.com>
2023-06-16 12:20:50 +0200

    Aligned footer elements center on mobile

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,15 +3,16 @@
     <div class="footer-content position-relative">
       <div class="container">
         <div class="row">
-            <div class="col-lg-4 col-md-5 order-md-3">
-                <div class="footer-info text-md-center">
-                  <a href="{{ route('home') }}" class="logo d-flex align-items-md-center justify-content-md-center">
+            <div class="col-lg-4 col-md-5 text-center text-md-start order-md-3">
+                <div class="footer-info text-center">
+                  <a href="{{ route('home') }}" class="logo d-flex align-items-center justify-content-center">
                     <x-logo/>
                   </a>
                   <p>Your safety is our number 1 priority</p>
                 </div>
             </div><!-- End footer info column-->
-            <div class="col-lg-2 col-md-4 footer-links order-md-4">
+            <div class="col-lg-2 col-md-4 text-center text-md-start footer-links order-md-4">
+                <h4 class="d-none d-md-block">Contact</h4>
                 <p>
                     A108 Adam Street <br>
                     NY 535022, USA<br><br>
@@ -19,8 +20,8 @@
                     <strong>Email:</strong> info@example.com<br>
                 </p>
             </div><!-- End footer links column-->
-            <div class="col-lg-2 col-md-3 footer-links order-md-1">
-                <h4>Company Links</h4>
+            <div class="col-lg-2 col-md-3 text-center text-md-start footer-links order-md-1">
+                <h4>Company</h4>
                 <ul>
                 <li><a href="{{ route('home') }}">Home</a></li>
                 <li><a href="{{ route('about') }}">About us</a></li>
@@ -30,7 +31,7 @@
                 </ul>
             </div><!-- End footer links column-->
 
-            <div class="col-lg-2 col-md-4 footer-links order-md-2">
+            <div class="col-lg-2 col-md-4  text-center text-md-start footer-links order-md-2">
                 <h4>Our Services</h4>
                 <ul>
                 <li><a href="#">Web Design</a></li>
@@ -41,10 +42,10 @@
             </div><!-- End footer links column-->
 
 
-            <div class="col-lg-2 col-md-4 footer-links order-md-5">
+            <div class="col-lg-2 col-md-4 text-center text-md-start footer-links order-md-5">
                     <h4>Follow us</h4>
                     <p>Follow {{ Config('app.name') }} online via our social channels for the latest news, updates, and insights.</p>
-                    <div class="social-links d-flex mt-3">
+                    <div class="social-links d-flex justify-content-center mt-3">
                         <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-linkedin"></i></a>
                         <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-facebook"></i></a>
                         <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-twitter"></i></a>
--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -5,7 +5,7 @@
       <div class="info d-flex align-items-center">
         <div class="container">
           <div class="row justify-content-center">
-            <div class="col-lg-8 text-center">
+            <div class="col col-lg-8 text-center">
               <h2 data-aos="fade-down">Welcome to <span>{{ config('app.name') }} </span></h2>
               <p data-aos="fade-up">Galactus Safety® your safety is our number 1 priority</p>
               <a data-aos="fade-up" data-aos-delay="200" href="#get-started" class="btn-get-started">Request A Quote</a>
