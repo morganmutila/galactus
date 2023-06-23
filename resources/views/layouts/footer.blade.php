2634529e627d68acbca7c6e4c626a709b4775158
Morgan Mutila <mutilamorgan@gmail.com>
2023-06-23 13:20:07 +0200

    Rebranded the logo

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,9 +6,9 @@
             <div class="col-lg-4 col-md-5 text-center text-md-start order-md-3">
                 <div class="footer-info text-center">
                   <a href="{{ route('home') }}" class="logo d-flex align-items-center justify-content-center">
-                    <x-logo/>
+                    <img src="{{ asset('img/logo.png') }}"/>
                   </a>
-                  <p>Your safety is our number 1 priority</p>
+                  <p>Your safety is our no 1 priority</p>
                 </div>
             </div><!-- End footer info column-->
             <div class="col-lg-2 col-md-4 text-center text-md-start footer-links order-md-4">
@@ -16,8 +16,8 @@
                 <p>
                     A108 Adam Street <br>
                     NY 535022, USA<br><br>
-                    <strong>Phone:</strong> +1 5589 55488 55<br>
-                    <strong>Email:</strong> info@example.com<br>
+                    <strong>Phone:</strong> +260976249140<br>
+                    <strong>Email:</strong> info@galactussafety.com<br>
                 </p>
             </div><!-- End footer links column-->
             <div class="col-lg-2 col-md-3 text-center text-md-start footer-links order-md-1">
@@ -26,6 +26,7 @@
                 <li><a href="{{ route('about') }}">About us</a></li>
                 <li><a href="{{ route('services') }}">Services</a></li>
                 <li><a href="{{ route('projects') }}">Projects</a></li>
+                <li><a href="{{ route('home') }}">Brand</a></li>
                 <li><a href="{{ route('blog') }}">Blog</a></li>
                 </ul>
             </div><!-- End footer links column-->
@@ -33,9 +34,9 @@
             <div class="col-lg-2 col-md-4  text-center text-md-start footer-links order-md-2">
                 <h4>Our Services</h4>
                 <ul>
-                <li><a href="#">Web Design</a></li>
-                <li><a href="#">Web Development</a></li>
-                <li><a href="#">Product Management</a></li>
+                <li><a href="#">Consultation</a></li>
+                <li><a href="#">Safety Gear</a></li>
+                <li><a href="#">Project Management</a></li>
                 <li><a href="#" class="btn py-3 rounded-1 aos-init aos-animate">Request a quote</a></li>
                 </ul>
             </div><!-- End footer links column-->
@@ -47,7 +48,7 @@
                     <div class="social-links d-flex justify-content-center mt-3">
                         <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-linkedin"></i></a>
                         <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-facebook"></i></a>
-                        {{-- <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-twitter"></i></a> --}}
+                        <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-twitter"></i></a>
                         <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-instagram"></i></a>
                     </div>
                 </div>
--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -1,33 +1,33 @@
-  <section id="hero" class="hero">
+<section id="hero" class="hero">
 
     <div id="hero-carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
 
-      <div class="info d-flex align-items-center">
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-lg-8 text-center">
-              <h2 data-aos="fade-down">Welcome to <span>{{ config('app.name') }} </span></h2>
-              <p data-aos="fade-up">Galactus Safety® your safety is our number 1 priority</p>
-              <a data-aos="fade-up" data-aos-delay="200" href="#get-started" class="btn-get-started d-none">Request A Quote</a>
+        <div class="info d-flex align-items-center">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 data-aos="fade-down">Welcome to <span>{{ config('app.name') }} </span></h2>
+                        <p data-aos="fade-up">Galactus Safety® your safety is our number 1 priority</p>
+                        <a data-aos="fade-up" data-aos-delay="200" href="#get-started" class="btn-get-started d-none">Request A Quote</a>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
 
-      <div class="carousel-item" style="background-image: url({{ asset('img/hero-carousel/hero-carousel-1.jpg') }})"></div>
-      <div class="carousel-item active" style="background-image: url({{ asset('img/hero-carousel/hero-carousel-2.jpg') }})"></div>
-      <div class="carousel-item" style="background-image: url({{ asset('img/hero-carousel/hero-carousel-3.jpg') }})"></div>
-      <div class="carousel-item" style="background-image: url({{ asset('img/hero-carousel/hero-carousel-4.jpg') }})"></div>
-      <div class="carousel-item" style="background-image: url({{ asset('img/hero-carousel/hero-carousel-5.jpg') }})"></div>
+        <div class="carousel-item" style="background-image: url({{ asset('img/hero-carousel/hero-carousel-1.jpg') }})"></div>
+        <div class="carousel-item active" style="background-image: url({{ asset('img/hero-carousel/hero-carousel-2.jpg') }})"></div>
+        <div class="carousel-item" style="background-image: url({{ asset('img/hero-carousel/hero-carousel-3.jpg') }})"></div>
+        <div class="carousel-item" style="background-image: url({{ asset('img/hero-carousel/hero-carousel-4.jpg') }})"></div>
+        <div class="carousel-item" style="background-image: url({{ asset('img/hero-carousel/hero-carousel-5.jpg') }})"></div>
 
-      <a class="carousel-control-prev d-none d-md-flex" href="#hero-carousel" role="button" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
-      </a>
+        <a class="carousel-control-prev d-none d-md-flex" href="#hero-carousel" role="button" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+        </a>
 
-      <a class="carousel-control-next d-none d-md-flex" href="#hero-carousel" role="button" data-bs-slide="next">
-        <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
-      </a>
+        <a class="carousel-control-next d-none d-md-flex" href="#hero-carousel" role="button" data-bs-slide="next">
+            <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+        </a>
 
     </div>
 
-  </section>
+</section>
