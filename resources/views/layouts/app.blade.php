e838c38e2cb072b314572ffb446c941bbcaeed7b
Morgan Mutila <mutilamorgan@gmail.com>
2023-06-26 12:05:27 +0200

    Improved performance by requiring files as they are needed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,57 +2,60 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>@yield('title', 'Galactus® - Your safety is our number 1 priority')</title>
-  <meta content="author" name="Mostek Technologies">
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>@yield('title', 'Galactus® - Your safety is our no 1 priority')</title>
+    <meta content="author" name="Mostek Technologies">
 
-  <!-- Favicons -->
-  <link href="{{ asset('img/favicon.png') }}" rel="icon">
-  <link href="{{ asset('img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+    <!-- Favicons -->
+    <link href="{{ asset('img/favicon.png') }}" rel="icon">
+    <link href="{{ asset('img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
-  <link href="https://fonts.cdnfonts.com/css/galano-grotesque-alt" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/galano-grotesque-alt" rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/aos/aos.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+    <!-- Vendor CSS & JS Files -->
+    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    @if (request()->RouteIs('gallary'))
+        <link href="{{ asset('vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+    @endif
+    @if (request()->RouteIs('home'))
+        <link href="{{ asset('vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+    @endif
 
-  <!-- Template Main CSS File -->
-  <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <!-- Main CSS File -->
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+
 </head>
-
 <body>
 
-  <!-- ======= Header ======= -->
-  @include('layouts.header')
-  <!-- End Header -->
+    <!-- ======= Header ======= -->
+    @include('layouts.header')
+    <!-- End Header -->
 
 
-  @yield('content')
+    @yield('content')
 
 
-  <!-- ======= Footer ======= -->
-  @include('layouts.footer')
-  <!-- End Footer -->
+    <!-- ======= Footer ======= -->
+    @include('layouts.footer')
+    <!-- End Footer -->
 
-  <x-scroll-to-top/>
+    <x-scroll-to-top/>
 
-  <div id="preloader"></div>
+    <div id="preloader"></div>
 
-  <!-- Vendor JS Files -->
-  <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('vendor/aos/aos.js') }}"></script>
-  <script src="{{ asset('vendor/glightbox/js/glightbox.min.js') }}"></script>
-  <script src="{{ asset('vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
-  <script src="{{ asset('vendor/swiper/swiper-bundle.min.js') }}"></script>
-  <script src="{{ asset('vendor/purecounter/purecounter_vanilla.js') }}"></script>
+    <!-- Vendor JS Files -->
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('vendor/aos/aos.js') }}"></script>
+    <script src="{{ asset('vendor/glightbox/js/glightbox.min.js') }}"></script>
+    <script src="{{ asset('vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('vendor/purecounter/purecounter_vanilla.js') }}"></script>
 
-  <!-- Template Main JS File -->
-  <script src="{{ asset('js/main.js') }}"></script>
+    <!-- Main JS File -->
+    <script src="{{ asset('js/main.js') }}"></script>
 
 </body>
 
